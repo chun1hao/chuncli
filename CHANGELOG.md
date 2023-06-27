ef90f75d18df892ed6513fb3c258fec78a5cc265
chunhao <chunhao1122@gmail.com>
2023-06-27 22:06:33 +0800

    feat(0627): 增加start命令、监听config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
-## 1.0.2 (2023-06-25)
+## 1.0.2 (2023-06-26)
+
 
 ### Features
 
-- **0625:** t ([a6f1854](https://github.com/chun1hao/chuncli/commit/a6f1854f04c2fd50bbbceb4c19c37cb64502adf6))
-- initialize ([1ac3876](https://github.com/chun1hao/chuncli/commit/1ac387671ef0d9973e29717e98867125916242d5))
+* **0626:** 基础配置修改 ([1fca470](https://github.com/chun1hao/chuncli/commit/1fca470dc64ec23ac74cbdddacae3b1524c8557a))
+* initialize ([1ac3876](https://github.com/chun1hao/chuncli/commit/1ac387671ef0d9973e29717e98867125916242d5))
+
+
+
--- bin/index.js
+++ bin/index.js
@@ -4,59 +4,79 @@ const chalk = require('chalk')
 const figlet = require('figlet')
 
 const pk = require('../package.json')
-const { THRESHOLD } = require('../lib/constant')
+const { THRESHOLD, MIN_NODE_VERSION } = require('../lib/constant')
+const checkNodeVersion = require('../lib/utils/checkNodeVesion')
 const enhanceErrorMessage = require('../lib/utils/enhanceErrorMessage')
 const log = require('../lib/utils/log')
 const { suggestCommands } = require('../lib/utils/index')
 const create = require('../lib/create/create')
+const start = require('../lib/start/startServe')
 
-program
-  .name('chbuild')
-  .description('一个脚手架')
-  .version(pk.version, '-v, --version')
+;(async () => {
+  try {
+    if (!checkNodeVersion(MIN_NODE_VERSION)) {
+      log.error(
+        `node版本过低，更新版本 ${chalk.yellow('>=' + MIN_NODE_VERSION)} `
+      )
+      return
+    }
 
-program
-  .command('create')
-  .description('创建一个模版项目')
-  .argument('<templateName>', '模版类型')
-  .argument('<projectName>', '项目名称')
-  .option('-f, --force', '存在同名文件夹强制覆盖')
-  .action((templateName, projectName, option) => {
-    create(templateName, projectName, option)
-  })
+    program
+      .name('chbuild')
+      .description('一个脚手架')
+      .version(pk.version, '-v, --version')
 
-// 非法命令处理
-program.arguments('<command>').action((cmd) => {
-  program.outputHelp()
-  console.log()
-  log.error(chalk.red(`未知命令 ${chalk.yellow(cmd)}`))
-  console.log()
-  suggestCommands(cmd, program.commands, THRESHOLD)
-})
+    program
+      .command('create')
+      .description('创建一个模版项目')
+      .argument('<templateName>', '模版类型')
+      .argument('<projectName>', '项目名称')
+      .option('-f, --force', '存在同名文件夹强制覆盖')
+      .action(create)
 
-// 增加说明
-program.on('--help', () => {
-  // 使用 figlet 绘制 Logo
-  console.log(
-    '\r\n' +
-      figlet.textSync(pk.name, {
-        font: 'Ghost',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
-        width: 80,
-        whitespaceBreak: true
-      })
-  )
-  console.log(
-    `\r\nRun ${chalk.cyan(
-      `chcli <command> --help`
-    )} for detailed usage of given command\r\n`
-  )
-})
+    program
+      .command('start')
+      .description('启动服务')
+      .option('-c, --config <config>', '自定义配置文件')
+      .option('-p, --port <port>', '端口号')
+      .action(start)
 
-// 重写参数缺失错误信息
-enhanceErrorMessage('missingArgument', (argsName) => {
-  return `缺少参数 ${chalk.yellow(`<${argsName}>`)}`
-})
+    // 非法命令处理
+    program.arguments('<command>').action((cmd) => {
+      program.outputHelp()
+      console.log()
+      log.error(chalk.red(`未知命令 ${chalk.yellow(cmd)}`))
+      console.log()
+      suggestCommands(cmd, program.commands, THRESHOLD)
+    })
 
-program.parse(process.argv)
+    // 增加说明
+    program.on('--help', () => {
+      // 使用 figlet 绘制 Logo
+      console.log(
+        '\r\n' +
+          figlet.textSync(pk.name, {
+            font: 'Ghost',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 80,
+            whitespaceBreak: true
+          })
+      )
+      console.log(
+        `\r\nRun ${chalk.cyan(
+          `chcli <command> --help`
+        )} for detailed usage of given command\r\n`
+      )
+    })
+
+    // 重写参数缺失错误信息
+    enhanceErrorMessage('missingArgument', (argsName) => {
+      return `缺少参数 ${chalk.yellow(`<${argsName}>`)}`
+    })
+
+    program.parse(process.argv)
+  } catch (e) {
+    log.error(e)
+  }
+})()
